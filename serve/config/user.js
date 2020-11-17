6b075ce126a16f627010a16043e574310594e89b
caodongdong <cdd.cao@fengniaojx.com>
2020-11-17 16:53:23 +0800

    自增

--- serve/config/user.js
+++ serve/config/user.js
@@ -1,7 +1,6 @@
 //关于用户表的增删改查
-let counter = 0
 let user = {
-    insert: "insert into ADD_USER (id,name,password) VALUES ('+ `${counter++}` +',?,?)" ,
+    insert: "insert into ADD_USER (name,password) VALUES (?,?)" ,
     query: "select * from ADD_USER",
     queryName: "select * from ADD_USER WHERE name = ? and password = ? ",
     update: "update ADD_USER SET name=?,password=? WHERE id=?",
